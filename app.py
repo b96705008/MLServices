cd188ff301999a5ab3cf1b0b47af323e3719db25
rongqichen <javawokr@gmail.com>
2016-12-25 13:18:46 +0800

    [Feature] - use ConfigParser instead of hard-code

--- app.py
+++ app.py
@@ -2,47 +2,40 @@
 
 import os
 import click
+import ConfigParser
 
 from flask import Flask
-
 from utils.env import root_dir
 
-options = {"iris": {"class_dataset": "IrisDataset",
-                    "class_algo": "IrisDNN",
-                    "class_builder": "IrisBuilder",
-                    "class_engine": "IrisEngine",
-                    "class_model": "IrisModel",
-                    "dataset": "iris.csv",
-                    "model": "iris_dnn",
-                    "location": "iris_classifier",
-                    "channel_builder": "iris_builder",
-                    "channel_api": "iris_api"}}
-
 @click.command()
+@click.option("-c", "--config", required=True)
 @click.option("-s", "--service", required=True)
 @click.option("-f", "--func", type=click.Choice(["builder", "api"]))
 @click.option("-p", "--port", default=5001)
 @click.option("-v", "--debug", default=False)
-def run(service, func, port, debug):
-    service_name = options[service]["location"]
+def run(config, service, func, port, debug):
+    cfg = ConfigParser.RawConfigParser()
+    cfg.read(config)
 
-    dataset_path = os.path.join(root_dir(), "datasets", options[service]["dataset"])
-    model_path = os.path.join(root_dir(), "models", options[service]["model"])
+    service_name = cfg.get("path", "location")
+
+    dataset_path = os.path.join(root_dir(), "datasets", cfg.get("path", "dataset"))
+    model_path = os.path.join(root_dir(), "models", cfg.get("path", "model"))
 
     mod = __import__("services.{}.builder.dataset".format(service_name),
                      fromlist=["services.{}.builder".format(service_name)])
-    class_dataset = getattr(mod, options[service]["class_dataset"])
+    class_dataset = getattr(mod, cfg.get("class", "dataset"))
 
     mod = __import__("services.{}.builder.algorithm".format(service_name),
                      fromlist=["services.{}.builder".format(service_name)])
-    class_algo = getattr(mod, options[service]["class_algo"])
+    class_algo = getattr(mod, cfg.get("class", "algorithm"))
 
     mod = __import__("services.{}.api.model".format(service_name),
                      fromlist=["services.{}.api".format(service_name)])
-    class_model = getattr(mod, options[service]["class_model"])
+    class_model = getattr(mod, cfg.get("class", "model"))
 
-    channel_builder = options[service]["channel_builder"]
-    channel_api = options[service]["channel_api"]
+    channel_builder = cfg.get("channel", "builder")
+    channel_api = cfg.get("channel", "api")
     channels = [channel_builder, channel_api]   # channels[0]: for Builder, channels[1]: after building model, inform whom
 
     # start service
@@ -51,7 +44,7 @@ def run(service, func, port, debug):
         builder = __import__("services.{}.builder.builder".format(service_name),
                              fromlist=["services.{}.builder".format(service_name)])
 
-        tbuilder = getattr(builder, options[service]["class_builder"])(class_dataset, dataset_path, class_algo, model_path, channels)
+        tbuilder = getattr(builder, cfg.get("class", "builder"))(class_dataset, dataset_path, class_algo, model_path, channels)
         tbuilder.build()
         tbuilder.run()
 
@@ -60,12 +53,12 @@ def run(service, func, port, debug):
         engine = __import__("services.{}.api.engine".format(service_name),
                              fromlist=["services.{}.engine".format(service_name)])
 
-        tengine = getattr(engine, options[service]["class_engine"])(dataset_path, class_model, model_path, [channel_api])
+        tengine = getattr(engine, cfg.get("class", "engine"))(dataset_path, class_model, model_path, [channel_api])
         tengine.run()
 
         mod = __import__("services.{}.api.service".format(service_name),
                          fromlist=["services.{}.service".format(service_name)])
-        service = mod.get_service(tengine, service_name = options[service]["location"])
+        service = mod.get_service(tengine, service_name)
 
         app = Flask(service)
         app.register_blueprint(service)
