94bbb80931822cd3bb966210e0b29734cf881684
rongqichen <javawokr@gmail.com>
2016-12-26 09:39:26 +0800

    [File] - refactor the class structure

---
+++ basic/model.py
@@ -0,0 +1,42 @@
+import redis
+import tensorflow as tf
+from keras import backend as K
+
+from utils.env import logger
+
+
+class MLModel(object):
+    def __init__(self, model_path):
+        self.model_path = model_path
+
+        self.load()
+
+    def load(self):
+        # assign the session to the attribute when the backend is 'tensorflow'
+        if K._BACKEND == "tensorflow":
+            self.session = tf.Session()
+            K.set_session(self.session)
+
+            logger.info("use the tensorflow to be the backend, we should set session firstly")
+
+        self.load_model()
+
+    def load_model(self):
+        raise NotImplementedError
+
+    def predict_probs(self, features):
+        raise NotImplementedError
+
+    def predict_class(self, features):
+        raise NotImplementedError
+
+    def before_reload_model(self):
+        if K._BACKEND == "tensorflow" \
+            and hasattr(self, "model") \
+            and hasattr(self.model, "session"):
+
+            logger.info("due to the tensorflow, before reloading model, we should close the session firstly")
+
+            self.model.session.close()
+        else:
+            pass
