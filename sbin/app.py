2b298205568f95f77a4b1d5e3ba345f503876243
rongqichen <javawokr@gmail.com>
2016-12-28 19:39:45 +0800

    [Feature] - separte the configuration of ml_service.cfg. one is for API; the other is for mining

--- sbin/app.py
+++ sbin/app.py
@@ -11,19 +11,17 @@ from utils.env import root_dir, data_dir, init_spark_context, logger
 
 @click.command()
 @click.option("-c", "--service", required=True)
-@click.option("-f", "--func", type=click.Choice(["builder", "api"]))
+@click.option("-f", "--func", type=click.Choice(["mining", "api"]))
 @click.option("-v", "--debug", default=False)
 def run(service, func, debug):
     basepath = os.path.join(data_dir(), service)
-    filepath_cfg = os.path.join(basepath, "ml_service.cfg")
+    filepath_cfg = os.path.join(basepath, "{}.cfg".format(func))
 
     cfg = ConfigParser.RawConfigParser()
     cfg.read(filepath_cfg)
 
     service_name = cfg.get("path", "location")
 
-    dataset_api_path = os.path.join(basepath, "dataset", cfg.get("path", "dataset_api"))
-    dataset_builder_path = os.path.join(basepath, "dataset", cfg.get("path", "dataset_builder"))
     model_path = os.path.join(basepath, "model", cfg.get("path", "model"))
 
     if cfg.has_option("setting", "spark_mode") and cfg.getboolean("setting", "spark_mode"):
@@ -32,44 +30,42 @@ def run(service, func, debug):
 
     listener = None
 
-    mod = __import__("services.{}.builder.dataset".format(service_name),
-                     fromlist=["services.{}.builder".format(service_name)])
-    class_dataset = getattr(mod, cfg.get("class", "dataset"))
-
-    mod = __import__("services.{}.builder.algorithm".format(service_name),
-                     fromlist=["services.{}.builder".format(service_name)])
-    class_algo = getattr(mod, cfg.get("class", "algorithm"))
-
-    mod = __import__("services.{}.common.model".format(service_name),
-                     fromlist=["services.{}.api".format(service_name)])
-    class_model = getattr(mod, cfg.get("class", "model"))
-
-    channel_builder = cfg.get("channel", "builder")
-    channel_api = cfg.get("channel", "api")
-    channels = [channel_builder, channel_api]   # channels[0]: for Builder, channels[1]: after building model, inform whom
-
     # start service
-    if func == 'builder':
-        # import dynamic modules
-        builder = __import__("services.{}.builder.builder".format(service_name),
-                             fromlist=["services.{}.builder".format(service_name)])
+    if func == 'mining':
+        dataset_builder_path = os.path.join(basepath, "dataset", cfg.get("path", "dataset_builder"))
 
+        mod = __import__("services.{}.mining.dataset".format(service_name), fromlist=["services.{}.mining".format(service_name)])
+        class_dataset = getattr(mod, cfg.get("class", "dataset"))
+
+        mod = __import__("services.{}.mining.algorithm".format(service_name), fromlist=["services.{}.mining".format(service_name)])
+        class_algo = getattr(mod, cfg.get("class", "algorithm"))
+
+        channel_builder = cfg.get("channel", "mining")
+        channel_api = cfg.get("channel", "api")
+        channels = [channel_builder, channel_api]   # channels[0]: for Builder, channels[1]: after building model, inform whom
+
+        # import dynamic modules
+        builder = __import__("services.{}.api.builder".format(service_name), fromlist=["services.{}.api".format(service_name)])
         tbuilder = getattr(builder, cfg.get("class", "builder"))(class_dataset, dataset_builder_path, class_algo, model_path,\
                            channels=channels, listener=listener)
         tbuilder.build()
         tbuilder.run()
 
     elif func == 'api':
+        dataset_api_path = os.path.join(basepath, "dataset", cfg.get("path", "dataset_api"))
+        channel_api = cfg.get("channel", "api")
+
+        mod = __import__("services.{}.common.model".format(service_name), fromlist=["services.{}.common".format(service_name)])
+        class_model = getattr(mod, cfg.get("class", "model"))
+
         # import dynamic modules
-        engine = __import__("services.{}.api.engine".format(service_name),
-                             fromlist=["services.{}.api".format(service_name)])
+        engine = __import__("services.{}.api.engine".format(service_name), fromlist=["services.{}.api".format(service_name)])
 
         tengine = getattr(engine, cfg.get("class", "engine"))(dataset_api_path, class_model, model_path,\
                           channel=[channel_api], listener=listener)
         tengine.start()
 
-        mod = __import__("services.{}.api.serving".format(service_name),
-                         fromlist=["services.{}.api".format(service_name)])
+        mod = __import__("services.{}.api.serving".format(service_name), fromlist=["services.{}.api".format(service_name)])
         service = mod.get_service(tengine, service_name)
 
         app = Flask(service_name)
@@ -77,6 +73,8 @@ def run(service, func, debug):
 
         port = cfg.getint("setting", "port")
         app.run(port=port, debug=debug)
+    else:
+        raise NotImplementedError
 
 if __name__ == "__main__":
     run()
