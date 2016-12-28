14cd66a9d447a81f253ea7b41570891b3e4089b8
rongqichen <javawokr@gmail.com>
2016-12-28 19:59:11 +0800

    [File] - upload the missing files

---
+++ lib/services/iris_classifier/mining/dataset.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+from basic.dataset import MLDataset
+
+
+class IrisDataset(MLDataset):
+    def init(self):
+        pass
+
+    def prepare_data(self):
+        iris = pd.read_csv(self.dataset_path)
+        self.X = iris.values[:, :4]
+        self.Y = iris.values[:, 4]
