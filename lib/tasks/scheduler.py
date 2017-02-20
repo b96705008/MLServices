3d8dba62cd175c1b21e96d158fd8a519c9f33153
Tai-Yi Kuo <b96705008@gmail.com>
2017-02-20 16:11:32 +0800

    tasks sample

---
+++ lib/tasks/scheduler.py
@@ -0,0 +1,32 @@
+import time
+
+from basic.event import EVENT_TRAIN
+from basic.listener import MLListener
+
+
+class MovieSheduler(object):
+	
+	def __init__(self):
+		self.listener = MLListener([])
+		self.channels = ['movie_mining']
+
+	def get_data(self):
+		print('get data...')
+		
+		time.sleep(3)
+
+		print('trigger mining...')
+		for c in self.channels:
+			self.listener.publish(c, EVENT_TRAIN)
+	
+
+if __name__ == '__main__':
+	# python lib/tasks/scheduler.py
+
+	i = 0
+	while True:
+		i += 1
+		print('Ready to schedule {} times'.format(i))
+		scheduler = MovieSheduler()
+		scheduler.get_data()
+		time.sleep(20)
